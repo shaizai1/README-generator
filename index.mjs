b941b4e4efe78609524145ad4bcb5f158d2e9d7d
shaizai1 <117948803+shaizai1@users.noreply.github.com>
2023-02-17 15:09:15 +0000

    structured prompts

--- index.mjs
+++ index.mjs
@@ -1,42 +1,63 @@
 import inquirer from "inquirer";
 import fs from "fs/promises";
 
-const {first_name, last_name, location, bio, Linkedin, Github} =
+const {Description, Contents, Installation, Usage, License, Contributing, Tests, Questions} =
  await inquirer
  .prompt([
     {
-        name:'',
-        message: "",
+        name:'Description',
+        message: "Description of project:",
         type: '',
+        default(){return '';},
     },
     {
-        name:'',
+        name:'Table of contents',
+        message: "Do you require a table of contents?",
+        type: 'input',
+        default(){return '';},
+    },
+    {
+        name:'Installation',
         message: "?",
         type: 'input',
         default(){return '';},
     },
     {
-        name:'',
-        message: "?",
-        type: 'input',
-    },
-    {
-        name:'',
+        name:'Usage',
         message: ":",
         type: 'input',
+        default(){return '';},
     },
     {
-        name:'',
+        name:'License',
         message: "?",
         type: 'input',
+        default(){return '';},
     },
     {
-        name:'',
+        name:'Contributing',
         message: "?",
         type: 'input',
+        default(){return '';},
+    },
+    {
+        name:'Tests',
+        message: "?",
+        type: 'input',
+        default(){return '';},
+    },
+    {
+        name:'Questions',
+        message: "?",
+        type: 'input',
+        default(){return '';},
     },
 ])
 
-let readmeText = ``
+let readmeText = `
+# The largest heading
+## The second largest heading
+###### The smallest heading
+`
 
-fs.writeFile("README.md",readmeText)
+fs.writeFile("README.md",readmeText);
